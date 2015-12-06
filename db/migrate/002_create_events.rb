36d479bcd8c69b99a5b8799affb45559d930e4b5
shuhei kinukawa <kinushu@gmail.com>
2015-12-06 15:50:25 +0900

    add event table's fields

--- db/migrate/002_create_events.rb
+++ db/migrate/002_create_events.rb
@@ -3,6 +3,9 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :title
       t.datetime :start_at
+      t.datetime :opening_at
+      t.datetime :release_at
+      t.integer :visible
       t.text :body
       t.timestamps
     end
--- models/event.rb
+++ models/event.rb
@@ -1,3 +1,12 @@
 class Event < ActiveRecord::Base
 
+# title
+# place 会場
+# start_at 開始時刻
+# opening_at オープン時間
+# release_at 公開日
+# visible 公開・非公開
+# body 本文
+# 
+
 end
